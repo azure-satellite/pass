ef45539f9ad473b0fb10e05e143053cb78cdf43b
Alejandro Hernandez <alejandro@peggedsoftware.com>
2017-11-11 21:55:33 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -4,8 +4,3 @@ cd $(dirname "$0")
 
 gpg --import key/store{,.pub}
 gpg --edit-key $(cat .gpg-id) trust quit
-export PASSWORD_STORE_DIR=$PWD
-
-pass git remote add origin git@github.com:stellarhoof/pass.git
-pass git fetch
-pass git branch -u origin/master
